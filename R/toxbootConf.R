a44734241f3e6e6573086b00cd69bdc867053b2e
Eric Dean Watt <eric.d.watt@gmail.com>
2016-09-15 18:11:34 -0400

    Updates to toxbootConf() and related functions to change DBNS to collection and specify a port.

--- R/toxbootConf.R
+++ R/toxbootConf.R
@@ -1,12 +1,18 @@
 #' @rdname toxboot_config_funcs
 #' @export
-toxbootConf <- function (mongo_host = NULL, DBNS = NULL, user = NULL, pass = NULL, db = NULL) {
+toxbootConf <- function (mongo_host = NULL,
+                         collection = NULL,
+                         user = NULL,
+                         pass = NULL,
+                         db = NULL,
+                         port = NULL) {
 
   if (!is.null(mongo_host)) options("TOXBOOT_HOST" = mongo_host)
-  if (!is.null(DBNS)) options("TOXBOOT_DBNS" = DBNS)
+  if (!is.null(collection)) options("TOXBOOT_COLLECTION" = collection)
   if (!is.null(user)) options("TOXBOOT_USER" = user)
   if (!is.null(pass)) options("TOXBOOT_PASS" = pass)
   if (!is.null(db)) options("TOXBOOT_DB" = db)
+  if (!is.null(port)) options("TOXBOOT_PORT" = port)
 
 }
 
@@ -22,14 +28,16 @@ toxbootConfSave <- function () {
 
   mongo_host <- options()$TOXBOOT_HOST
   mongo_host <- if(is.na(mongo_host)) shQuote("NA_character_") else shQuote(mongo_host)
-  DBNS <- options()$TOXBOOT_DBNS
-  DBNS <- if(is.na(DBNS)) shQuote("NA_character_") else shQuote(DBNS)
+  collection <- options()$TOXBOOT_COLLECTION
+  collection <- if(is.na(collection)) shQuote("NA_character_") else shQuote(collection)
   user <- options()$TOXBOOT_USER
   user <- if(is.na(user)) shQuote("NA_character_") else shQuote(user)
   pass   <- options()$TOXBOOT_PASS
-  pass   <- if(is.na(pass))   shQuote("NA_character_")   else shQuote(pass)
+  pass   <- if(is.na(pass))   shQuote("NA_character_") else shQuote(pass)
   db   <- options()$TOXBOOT_DB
-  db   <- if(is.na(db))   shQuote("NA_character_")   else shQuote(db)
+  db   <- if(is.na(db))   shQuote("NA_character_") else shQuote(db)
+  port   <- options()$TOXBOOT_PORT
+  port   <- if(is.na(port))   shQuote("NA_character_") else shQuote(port)
 
   pkgd <- system.file(package = "toxboot")
   conf_file <- file.path(pkgd, "toxboot.config")
@@ -38,10 +46,11 @@ toxbootConfSave <- function () {
       "## See ?toxbootConf for more info about these settings.\n",
       "\n",
       "mongo_host =", mongo_host, "# the database server IP address", "\n",
-      "DBNS =", DBNS, "# the database server collection", "\n",
+      "collection =", collection, "# the database server collection", "\n",
       "user =", user, "# username for database db", "\n",
       "pass =", pass, "# password for username on db", "\n",
       "db   =", db, "# the database where the user is authenticated", "\n",
+      "port =", port, "# the port to connect on. Default for mongoDB is 27017", "\n",
       sep = " ",
       file = conf_file,
       append = FALSE)
@@ -59,10 +68,11 @@ toxbootConfReset <- function () {
       "## See ?toxbootConf for more info about these settings.",
       "",
       "mongo_host = NA_character_ # the database server IP address",
-      "DBNS = NA_character_ # the database server collection",
+      "collection = NA_character_ # the database server collection",
       "user = NA_character_ # username for database db",
       "pass = NA_character_ # password for username on db",
       "db = NA_character_ # the database where the user is authenticated",
+      "port = NA_character_ # the port to connect on. Default for mongoDB is 27017",
       sep = "\n",
       file = conf_file,
       append = FALSE)
@@ -78,14 +88,18 @@ toxbootConfLoad <- function () {
 
   source(conf_file, local = TRUE)
 
-  toxbootConf(mongo_host, DBNS, user, pass, db)
+  toxbootConf(mongo_host, collection, user, pass, db, port)
 }
 
 #' @rdname toxboot_config_funcs
 #' @export
 toxbootConfList <- function(show.pass = FALSE) {
 
-  opts <- list("TOXBOOT_HOST", "TOXBOOT_DBNS", "TOXBOOT_USER", "TOXBOOT_DB")
+  opts <- list("TOXBOOT_HOST",
+               "TOXBOOT_COLLECTION",
+               "TOXBOOT_USER",
+               "TOXBOOT_DB",
+               "TOXBOOT_PORT")
   if (show.pass) opts <- c(opts, "TOXBOOT_PASS")
   opt_list <- do.call(options, opts)
   return (opt_list)
