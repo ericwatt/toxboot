c92d3df0592e9e51bf7e0a82a7944e2cb62ec131
Eric Dean Watt <eric.d.watt@gmail.com>
2016-09-19 14:42:13 -0400

    More tests to track down travis-ci test failure.

--- tests/testthat/test_toxboot.R
+++ tests/testthat/test_toxboot.R
@@ -88,6 +88,27 @@ test_that("toxboot writes to and reads from MongoDB", {
   dat_memory <- dat[, !c("started", "modified"), with=FALSE]
   dat_mongo <- dat_fetch[, !c("started", "modified"), with=FALSE]
 
+  nmed_gtbls <- c(2, 2, 2, 2, 4, 4, 3, 3, 4, 3, 4, 4, 6, 6, 3, 4, 1, 2, 5, 5,
+                  0, 0, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0)
+  gnls_ers <- c(-1.807594985, -1.459023911, -1.573828628, -1.777077464, 2.40987324,
+                2.406260863, 2.191250177, 1.781406928, 1.167992319, 1.863780877,
+                2.160360009, 2.142468526, 2.057942036, 2.232296488, 2.242811639,
+                2.194470631, 1.549692612, 1.99877665, 2.287199183, 1.784798892,
+                NA, NA, NA, NA, 1.72576192, 1.705334943, NA, NA, 1.62146045,
+                2.007935596, 1.795829507, NA, NA, NA, NA, 1.731623176, NA, NA,
+                NA, NA)
+
+  gnls_er_sds <- c(0.282330867, 0.259495502, 0.404799878, NaN, 0.243378855, 0.247652604,
+                   NaN, NaN, NaN, 0.281104975, 0.221801601, 0.224600087, 0.279923346,
+                   0.265922126, NaN, NA, NaN, 0.215854774, NaN, 0.232327683, NA,
+                   NA, NA, NA, 0.268741279, 0.263008787, NA, NA, 0.224170319, NaN,
+                   0.216426479, NA, NA, NA, NA, 0.237256641, NA, NA, NA, NA)
+
+  expect_equal(nmed_gtbls, dat_mongo[, nmed_gtbl])
+  expect_equal(nmed_gtbls, dat_memory[, nmed_gtbl])
+  expect_equal(gnls_ers, dat_mongo[, gnls_er])
+  expect_equal(gnls_ers, dat_memory[, gnls_er])
+
 
   expect_equal(dat_memory, dat_mongo, tolerance = .0000001, scale = 1)
 })
